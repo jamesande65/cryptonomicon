2a9f154858f2914898f94341a9406247bcd5516b
dmitry.nasyrkulov@gmail.com <dmitry.nasyrkulov@gmail.com>
2021-05-30 10:26:32 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@/assets/styles/main.css";
+import "@/assets/styles/main.css";
+import "./assets/tailwind.css";
+
+createApp(App).mount("#app");
